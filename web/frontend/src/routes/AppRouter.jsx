b951e6d398daef9df297d47ed374ebb2bdac1a44
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-11-01 11:28:24 +0700

    add index file react

--- web/frontend/src/routes/AppRouter.jsx
+++ web/frontend/src/routes/AppRouter.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "../pages/Dashboard";
-import Diagnosis from "../pages/Diagnosis";
-import Tracking from "../pages/Tracking";
-import Explanation from "../pages/Explanation";
-import About from "../pages/About";
-import Navbar from "../components/Navbar";
+import Home from "../pages/index";
+// import Diagnosis from "../pages/Diagnosis";
+// import Tracking from "../pages/Tracking";
+// import Explanation from "../pages/Explanation";
+// import About from "../pages/About";
+// import Navbar from "../components/Navbar";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/diagnosis" element={<Diagnosis />} />
-        <Route path="/tracking" element={<Tracking />} />
-        <Route path="/explanation" element={<Explanation />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/diagnosis" element={<Diagnosis />} /> */}
+        {/* <Route path="/tracking" element={<Tracking />} /> */}
+        {/* <Route path="/explanation" element={<Explanation />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
   );
