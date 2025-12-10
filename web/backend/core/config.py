88b8c88ff3e9f8edf01ef07eb9ca0d966ac6ef8c
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-12-11 02:55:37 +0700

    merge main into my branch

--- web/backend/core/config.py
+++ web/backend/core/config.py
@@ -1,23 +1,20 @@
 import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
-
-class Settings:
-    PROJECT_NAME: str = "Diabetes Diagnosis System"
-    VERSION: str = "1.0.0"
-    API_V1_STR: str = "/api/v1"
+class Settings(BaseSettings):
+  # 
+  PROJECT_NAME: str = "Diabetes Diagnosis System"
+  VERSION: str = "1.0.0"
+  API_V1_STR: str = "/api/v1"
+  ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     
-    # Database - Thay đổi sang SQL Server
-    DATABASE_URL: str = os.getenv(
-        "DATABASE_URL", 
-        "mssql+pyodbc://@localhost/diabetes_db?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
-        # "mssql+pyodbc://sa:YourPassword@localhost/diabetes_db?driver=ODBC+Driver+17+for+SQL+Server"
-    )
+  ALGORITHM: str = "HS256"
     
-    # Security
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key-change-in-production")
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))  # 1h
+  SECRET_KEY: str
+  DATABASE_URL: str
+    
+  class Config:
+    env_file = ".env"
+    case_sensitive = True
 
 settings = Settings()
