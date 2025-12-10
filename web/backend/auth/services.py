21067f408fbbb2f8adaa7e9a35027bd086539c9c
Tranduong03 <106472477+Tranduong03@users.noreply.github.com>
2025-12-11 03:04:21 +0700

    delete pycache

--- web/backend/auth/services.py
+++ web/backend/auth/services.py
@@ -14,7 +14,7 @@ def get_user_by_id(db: Session, user_id: int):
     return db.query(User).filter(User.id == user_id).first()
 
 def get_all_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
+    return db.query(User).order_by(User.id).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: UserCreate):
     # Kiểm tra email đã tồn tại
